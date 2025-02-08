a0f57cefa5563d2edeb1040c1d4a547be3930600
rudra pratap singh <prataprudra10102001@gmail.com>
2025-02-09 03:18:28 +0530

    toDoList

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [toList, setToList] = useState("")
+  const [item, setItem] = useState([]);
+  function AddList(e) {
+    // console.log("added")
+    const todo = e.target.value;
+    console.log(todo)
+    // console.log(setToList())
+    // console.log(toList)
+
+    setToList(todo)
+
+  }
+
+  function ItemInsert() {
+    setItem((prev) => {
+      return [...prev, toList]
+    }
+    )
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input type="text" name="todo"
+          value={toList} onChange={AddList} />
+        <button type="submit"
+          onClick={ItemInsert}
+        >
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item</li>
+
+          {item.map((item) => <li>{item}</li>)}
+
         </ul>
       </div>
     </div>
