cecfe8f1bcaeb4136ca3947f9d69c47ffa64f154
rudra pratap singh <prataprudra10102001@gmail.com>
2025-02-12 00:15:46 +0530

    props added

--- src/components/InputArea.jsx
+++ src/components/InputArea.jsx
@@ -1,17 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
-function InputArea() {
-    const [toList, setToList] = useState("")
-    function AddList(e) {
-        const todo = e.target.value;
-        setToList(todo)
+function InputArea(props) {
+    const [inputText, setInputText] = useState("");
+    function handleChange(event) {
+        const newValue = event.target.value;
+        setInputText(newValue);
     }
-    
 
     return (
         <div className="form">
             <input onChange={handleChange} type="text" value={inputText} />
-            <button onClick={addItem}>
+            <button onClick={() => {
+                props.addItem(inputText);
+                setInputText("")
+            }}>
                 <span>Add</span>
             </button>
         </div>
